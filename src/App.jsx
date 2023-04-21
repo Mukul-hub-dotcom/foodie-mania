13be4fd1dc4bc75aa3b5becebb7abdb5019e11e3
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-04-21 17:57:45 +0530

    added bootstrap library and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import "./App.css";
 import Home from "./screen/Home";
 import Login from "./screen/Login";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 function App() {
   return (
     <div className="fs-1">
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </div>
-  )
-
+  );
 }
 
 export default App;
