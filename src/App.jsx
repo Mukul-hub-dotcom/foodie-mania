3e26d1979bb573e29a177405f20e71f069704100
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-05-02 09:12:45 +0530

    added path for my-order section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import "./App.css";
 import Home from "./screen/Home";
 import Login from "./screen/Login";
+import Signup from "./screen/Signup";
 import { Route, Routes } from "react-router-dom";
 import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import { CartProvider } from "./components/ContextReducer";
+import MyOrder from "./screen/MyOrder";
 function App() {
   return (
-    <div className="fs-1">
+    <CartProvider>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/create-user" element={<Signup />}></Route>
+        <Route path="/myOrder" element={<MyOrder />}></Route>
       </Routes>
-    </div>
+    </CartProvider>
   );
 }
 
